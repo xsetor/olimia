287b14b968552854729a6fce5007022690a69f94
xsetor <36992636+xsetor@users.noreply.github.com>
2023-06-12 18:59:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+window.addEventListener('load', function() {
+  const loader = document.querySelector('.loader');
+  const videoContainer = document.querySelector('.video-container');
+  const videos = Array.from(document.querySelectorAll('.background-video'));
+  const logo = document.querySelector('.logo');
+
+  let currentVideoIndex = 0;
+
+  function playNextVideo() {
+    const currentVideo = videos[currentVideoIndex];
+    const nextVideoIndex = (currentVideoIndex + 1) % videos.length;
+    const nextVideo = videos[nextVideoIndex];
+
+    currentVideo.style.display = 'none';
+    nextVideo.style.display = 'block';
+
+    currentVideo.pause();
+    currentVideo.currentTime = 0;
+
+    currentVideoIndex = nextVideoIndex;
+
+    nextVideo.play().then(() => {
+      currentVideo.style.display = 'none';
+    }).catch((error) => {
+      console.error('Error playing video:', error);
+    });
+  }
+
+  function handleClick(event) {
+    if (event.target !== logo) {
+      playNextVideo();
+    }
+  }
+
+  document.addEventListener('click', handleClick);
+
+  videos.forEach((video, index) => {
+    if (index !== 0) {
+      video.style.display = 'none';
+    }
+  });
+
+  videos[currentVideoIndex].play().catch((error) => {
+    console.error('Error playing video:', error);
+  });
+
+  setTimeout(function() {
+    loader.style.display = 'none';
+    videoContainer.style.display = 'block';
+  }, 100);
+});
