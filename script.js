90f9223bec36c01ea94459c8be36f84cf8a56014
xsetor <xsetor@gmail.com>
2023-06-15 22:25:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -65,33 +65,3 @@ window.addEventListener('load', function() {
   }, 100);
 });
 
-
-document.addEventListener("DOMContentLoaded", function() {
-  // Get the form and input elements
-  var form = document.querySelector("form");
-  var emailInput = document.getElementById("email-input");
-
-  // Add submit event listener to the form
-  form.addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
-
-    // Get the email value
-    var email = emailInput.value;
-
-    // Send the email using AJAX
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "subscribe.php", true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        var response = xhr.responseText;
-        if (response === "Success") {
-          alert("Email subscribed successfully!");
-        } else {
-          alert("Error subscribing to email.");
-        }
-      }
-    };
-    xhr.send("email=" + email);
-  });
-});
